46fd47fc8ed196a94f073e039f2cb423f1db8313
zaizai <327120052@qq.com>
2022-06-25 18:57:19 +0800

    20220625 Bug修复

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from corneakeeper import create_app
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
+from corneakeeper import create_app
 
 app = create_app(os.getenv('FLASK_ENV'))
